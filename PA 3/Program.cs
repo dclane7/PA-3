dab6869ed001bd7637e62367a61dc9548d08db3b
dclane <dclane@DESKTOP-F31AKFQ>
2019-10-14 17:57:52 -0700

    introduction

--- PA 3/Program.cs
+++ PA 3/Program.cs
@@ -14,7 +14,7 @@ namespace PA_3
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name + ", Welcome to Jolly Jackpot Land!");
             Console.WriteLine("We have two games for you to play: slot machine and dice!");
-            Console.ReadLine();
+            
         }
     }
 }
